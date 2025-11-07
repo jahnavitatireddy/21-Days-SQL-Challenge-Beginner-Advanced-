5cdefed0ce65d433c9314aa0d9e4355af0bad1d3
jahnavitatireddy <129886022+jahnavitatireddy@users.noreply.github.com>
2025-11-07 22:30:32 +0530

    Create Day-03.sql

---
+++ Day-03.sql
@@ -0,0 +1,12 @@
+-- Day 3 - SQL Challenge
+-- Question: Retrieve the top 5 weeks with the highest patient refusals across all services, showing week, service, patients_refused, and patients_request. Sort by patients_refused in descending order.
+
+SELECT week,
+       service,
+       patients_refused,
+       patients_request
+FROM services_weekly
+ORDER BY patients_refused DESC
+LIMIT 5;
+
+-- @DPDzero @indiandataclub #SQLWithIDC
