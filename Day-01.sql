9f75e0fc19ce2be731ab725ca234715cc96bc9ed
jahnavitatireddy <129886022+jahnavitatireddy@users.noreply.github.com>
2025-11-07 15:25:56 +0530

    Create Day-01.sql

---
+++ Day-01.sql
@@ -0,0 +1,6 @@
+-- Day 1 - SQL Challenge
+-- Question: List all unique hospital services available in the hospital.
+
+SELECT DISTINCT(service) FROM services_weekly;
+
+-- @DPDzero @indiandataclub #SQLWithIDC
